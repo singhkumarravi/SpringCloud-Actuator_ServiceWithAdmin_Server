771f09a28ad9181a8c56d2850bfa15ae4fc80618
Ravi <Ravi@DESKTOP-5A7HVVO>
2021-09-10 18:44:11 +0530

    91-D

---
+++ src/main/java/com/olive/SpringCloudActuatorServiceWithAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.olive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudActuatorServiceWithAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudActuatorServiceWithAdminServerApplication.class, args);
+	}
+
+}
